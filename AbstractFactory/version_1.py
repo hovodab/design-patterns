df2ded538a099521e3efeffbfd05b856110195f5
Hovhannes Dabaghyan <hovhannes.dabaghyan@gmail.com>
2017-09-20 21:45:01 +0400

    Abstract factory first try.

---
+++ AbstractFactory/version_1.py
@@ -0,0 +1,215 @@
+#!/usr/bin/python3
+
+import math
+from abc import ABCMeta, abstractmethod
+
+
+class FancyObject(object):
+    """
+    Fancy helper class.
+    """
+    @staticmethod
+    def fancy_print(message):
+        """
+        Print message in fancy way.
+
+        :type message: str
+        :param message: Message to be printed in fancy-mancy way.
+
+        :rtype: void
+        :return: void
+        """
+        pre_post = 50
+        spaces_length_left = math.ceil((pre_post - 2 - len(message)) / 2)
+        spaces_length_right = spaces_length_left - (2 * spaces_length_left + 2 + len(message) - pre_post)
+        print('=' * pre_post)
+        print("!" + " " * spaces_length_left + message + " " * spaces_length_right + "!")
+        print('=' * pre_post)
+
+
+class AbstractWing(FancyObject, metaclass=ABCMeta):
+    """
+    Wing functionality interface. Just show.
+    """
+    @abstractmethod
+    def show(self):
+        pass
+
+
+class AbstractTail(FancyObject, metaclass=ABCMeta):
+    """
+    Tail functionality interface. Just show.
+    """
+    @abstractmethod
+    def show(self):
+        pass
+
+
+class AbstractWheels(FancyObject, metaclass=ABCMeta):
+    """
+    Wheels functionality interface. Just show.
+    """
+    @abstractmethod
+    def show(self):
+        pass
+
+
+class FighterWing(AbstractWing):
+    """
+    Wing of fighter. With implemented functionality for fighter.
+    """
+    def show(self):
+        self.fancy_print("Fighter Wing ^")
+
+
+class FighterTail(AbstractTail):
+    """
+    Tail of fighter. With implemented functionality for fighter.
+    """
+    def show(self):
+        self.fancy_print("Fighter Tail I_I")
+
+
+class FighterWheels(AbstractWheels):
+    """
+    Wheels of fighter. With implemented functionality for fighter.
+    """
+    def show(self):
+        self.fancy_print("Fighter Wheels oo o")
+
+
+class PassengerAirplaneWing(AbstractWing):
+    """
+    Wing of passenger airplane. With implemented functionality for passenger airplane.
+    """
+    def show(self):
+        self.fancy_print("Passenger Airplanes Wing /\\")
+
+
+class PassengerAirplaneTail(AbstractTail):
+    """
+    Tail of passenger airplane. With implemented functionality for passenger airplane.
+    """
+    def show(self):
+        self.fancy_print("Passenger Airplanes Tail T")
+
+
+class PassengerAirplaneWheels(AbstractWheels):
+    """
+    Wheels of passenger airplane. With implemented functionality for passenger airplane.
+    """
+    def show(self):
+        self.fancy_print("Passenger Airplanes Wheels OO OO O")
+
+
+class AbstractAircraftFactory(metaclass=ABCMeta):
+    """
+    Aircraft factory. Just interfaces.
+    """
+    @abstractmethod
+    def create_wing(self):
+        """
+        Create wing for aircraft.
+        """
+        pass
+
+    @abstractmethod
+    def create_tail(self):
+        """
+        Create tail for aircraft.
+        """
+        pass
+
+    @abstractmethod
+    def create_wheels(self):
+        """
+        Create wheels for aircraft.
+        """
+        pass
+
+
+class FighterFactory(AbstractAircraftFactory):
+    """
+    Fighter aircraft factory.
+    """
+    def create_wing(self):
+        """
+        Create wing for fighter.
+
+        :rtype: FighterWing
+        :return: FighterWing object.
+        """
+        return FighterWing()
+
+    def create_tail(self):
+        """
+        Create tail for fighter.
+
+        :rtype: FighterTail
+        :return: FighterTail object.
+        """
+        return FighterTail()
+
+    def create_wheels(self):
+        """
+        Create wheels for fighter.
+
+        :rtype: FighterWheels
+        :return: FighterWheels object.
+        """
+        return FighterWheels()
+
+
+class PassengerAirplaneFactory(AbstractAircraftFactory):
+
+    def create_wing(self):
+        """
+        Create wing for passenger airplane.
+
+        :rtype: PassengerAirplaneWing
+        :return: PassengerAirplaneWing object.
+        """
+        return PassengerAirplaneWing()
+
+    def create_tail(self):
+        """
+        Create wing for passenger airplane.
+
+        :rtype: PassengerAirplaneTail
+        :return: PassengerAirplaneTail object.
+        """
+        return PassengerAirplaneTail()
+
+    def create_wheels(self):
+        """
+        Create wing for passenger airplane.
+
+        :rtype: PassengerAirplaneWheels
+        :return: PassengerAirplaneWheels object.
+        """
+        return PassengerAirplaneWheels()
+
+
+def main():
+    print("**************************************************")
+    print()
+    aircraft1 = PassengerAirplaneFactory()
+    aircraft2 = FighterFactory()
+
+    aircraft1.create_wing().show()
+    aircraft1.create_tail().show()
+    aircraft1.create_wheels().show()
+
+    print()
+    print()
+
+    aircraft2.create_wing().show()
+    aircraft2.create_tail().show()
+    aircraft2.create_wheels().show()
+
+    print()
+    print("**************************************************")
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # design-patterns
 design patterns in python
+
+---------------------------
+Self Educational Repository
+
+I know that there could be many repositories that already contain design patterns implementations in python,
+but I would like to create one more for practicing purposes. Also it could help to someone.
+
+-------------
+Requirements:
+
+* python 3
+* linux like system(haven't tried it on Windows, and I'm not going...)
